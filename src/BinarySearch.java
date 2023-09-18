a50adea00c3fea26ff2609000c86070d2681aa9c
binary-river <66468384+binary-river@users.noreply.github.com>
2023-09-18 18:22:32 +0900

    first source

---
+++ src/BinarySearch.java
@@ -0,0 +1,13 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+package src;
+
+public class BinarySearch {
+    public static void main(String[] args) {
+
+    }
+}
