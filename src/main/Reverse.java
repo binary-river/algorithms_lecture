4770690a27c086afa2d56adea3fe508062700487
binary-river <66468384+binary-river@users.noreply.github.com>
2024-01-08 18:34:00 +0900

    add reverse

---
+++ src/main/Reverse.java
@@ -0,0 +1,23 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+package src.main;
+
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<Integer>();
+
+        while (!StdIn.isEmpty())
+            stack.push(StdIn.readInt());
+
+        for (int i : stack)
+            StdOut.println(i);
+    }
+}
