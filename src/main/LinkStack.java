af3b64f7962ed07eec2ebec4ea31e079cc941f18
binary-river <66468384+binary-river@users.noreply.github.com>
2024-01-18 10:51:30 +0900

    linkstack update

--- src/main/LinkStack.java
+++ src/main/LinkStack.java
@@ -10,10 +10,8 @@ import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
 public class LinkStack<Item> {
-
     private int N;
     private Node first;
-    private Node last;
 
     public void push(Item item) {
         Node oldFirst = first;
